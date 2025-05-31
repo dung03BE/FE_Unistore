b6f050de1443e0e71791efa38be8c20de7af0855
dung03BE <dung2003hn@gmail.com>
2025-05-31 22:23:50 +0700

    u

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://server-uni-dbgg.onrender.com";
-export const BASE_URL = "http://localhost:8081";
+export const BASE_URL = "https://server-uni-dbgg.onrender.com";
+// export const BASE_URL = "http://localhost:8081";
